8ac41442018facc3dd396edd44cff4bacfba0992
Friedrich Gonzalez <friedrichg@gmail.com>
2012-03-20 11:07:06 +0100

    fix for README

--- one.sh
+++ one.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+# Creates a password string for /etc/shadow. (tested on RHEL6.2 SHA512"
 python -c "import crypt; password=raw_input('Password:'); salt=raw_input('Salt:'); print crypt.crypt(password,'\$6\$'+salt+'\$')"
 
