d1b26f9baf4824f82915226f0333856f8c0219fc
Friedrich Gonzalez <friedrichg@gmail.com>
2013-10-31 23:27:44 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
 #remove all kernels but current
-#taken from here http://markmcb.com/2013/02/04/cleanup-unused-linux-kernels-in-ubuntu/
+#taken from here http://ubuntugenius.wordpress.com/2011/01/08/ubuntu-cleanup-how-to-remove-all-unused-linux-kernel-headers-images-and-modules/
 # tested with ubuntu 12.10
 dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
