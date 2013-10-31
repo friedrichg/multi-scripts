975ef1ecdbd5a128be42d9407e0723371995526f
Friedrich Gonzalez <friedrichg@gmail.com>
2013-10-31 23:25:40 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
-#remove all kernels
+#remove all kernels but current
 #taken from here http://markmcb.com/2013/02/04/cleanup-unused-linux-kernels-in-ubuntu/
 # tested with ubuntu 12.10
 dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
