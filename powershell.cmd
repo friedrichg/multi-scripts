92aadfee10d45259b86f58980231146d3f391246
Friedrich Gonzalez <friedrichg@gmail.com>
2014-01-20 10:10:40 +0100

    Starting powershell script

--- powershell.cmd
+++ powershell.cmd
@@ -13,3 +13,9 @@ start /w pkgmgr /iu:"TelnetServer"
 #Remove telnet client or Server
 start /w pkgmgr /uu:"TelnetClient"
 start /w pkgmgr /uu:"TelnetServer"
+
+#Start powershell script
+#powershell ./Test.ps1 --> WRONG!!!
+powershell "powershell $(cat Test.ps1)"
+powershell "$(gc Test.ps1)"
+powershell -ExecutionPolicy Bypass ./Test.ps1
