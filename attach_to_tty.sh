68b8665e99d96414b7a2f1f5fd7ea6c381f8346b
Friedrich Gonzalez <friedrichg@gmail.com>
2014-01-23 12:36:02 +0100

    Create attach_to_tty.sh

---
+++ attach_to_tty.sh
@@ -0,0 +1,20 @@
+function tty_attach() {
+pid=$(ps -ef| grep $1|grep bash|awk {print\$2})
+gdb -p $pid << HEREDOC
+p dup2(open("$(tty)", 1), 1)
+detach
+quit
+HEREDOC
+}
+
+function tty_deattach() {
+tty=$(tty)
+pid=$(ps -ef| grep $1|grep bash|awk {print\$2})
+oldfds=$(lsof -nPp $pid|grep $tty|awk {print\$4}|grep -o '[0-9]*' 2>/dev/null)
+gdb -p $pid << HEREDOC
+$(echo -n "$oldfds"|sed 's|^.*$|p close(\0)|g' )
+p dup2(open("/dev/$1", 1), 1)
+detach
+quit
+HEREDOC
+}
