99a326af4d49a87582235206b0977faee93f4a35
Friedrich Gonzalez <friedrichg@gmail.com>
2014-05-25 19:00:39 +0200

    Update sql.sh

--- sql.sh
+++ sql.sh
@@ -4,4 +4,14 @@ mysqlbinlog <logfiles> | grep exec | grep end_log_pos | cut -d' ' -f2- | cut -d:
 #mysqlbinlog headers sorted by event time
 mysqlbinlog <logfiles> | grep exec | grep end_log_pos | grep -v exec_time=0 | sed 's/^\(.*exec_time=\([0-9]\+\).*\)/\2 - \1 /' | sort -n
 
+#Mysql extended status
+mysqladmin -u root -p extended-status
 
+#vacuum sqllite
+find ~ -name '*.sqlite' -exec sqlite3 '{}' 'VACUUM;' \;
+
+find ~ -name '*.sqlite' -exec sqlite3 '{}' 'VACUUM;' \;
+find ~ -name '*.sqlite' -exec sqlite3 '{}' 'REINDEX;' \;
+
+for %a in (*.sqlite) do (sqlite3 %a vacuum)
+for %a in (*.sqlite) do (sqlite3 %a reindex)
