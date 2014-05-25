069475a57ecb843b7f21fbc82f9a283bedfda086
Friedrich Gonzalez <friedrichg@gmail.com>
2014-05-25 19:03:27 +0200

    Update and rename strace.sh to debug_process.sh

--- debug_process.sh
+++ debug_process.sh
@@ -1,2 +1,5 @@
 #Slow down IO heavy process
 slow () { [ -n $1 ] && while kill -STOP $1; do sleep 1; kill -CONT $1; sleep 1; done }
+
+#intercept stdout/stderr of another process
+strace -ff -e trace=write -e write=1,2 -p SOME_PID
