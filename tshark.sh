08a408f32c3ec5cbf7c6cf8e43a212948c1cf48d
Friedrich Gonzalez <friedrichg@gmail.com>
2014-08-19 14:26:16 +0200

    Update tshark.sh

--- tshark.sh
+++ tshark.sh
@@ -11,3 +11,5 @@ tshark -o "tcp.desegment_tcp_streams:TRUE" -i eth0 -R "http.request or http.resp
 # show https
 tshark -p -i1 -o "ssl.desegment_ssl_records: TRUE" -o "ssl.keys_list:$ip,443,http,$key3" -o "ssl.debug_file:lala" -n -R 'tcp.port eq 443' -T fields -e ip.host -e tcp.port -e http.request.full_uri -e http.request.method -e http.response.code -e http.response.phrase -e http.content_length -e data -e text -E separator=\; 
 
+# remote session from windows
+"c:\Program Files\PuTTY\plink.exe" -ssh root@remotehost tcpdump -npi eth0 -s0 -w - port 80 | "c:\Program Files\Wireshark\Wireshark.exe" -k -i -
